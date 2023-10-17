8a0158000417dbcd1c9a1cd5045fbe97ff6ba6be
NARooshnavand <nasserakhoundi@gmail.com>
2023-10-17 00:41:57 -0700

    composition of cin with while

--- hello.cpp
+++ hello.cpp
@@ -8,11 +8,9 @@ int main()
     int age;
     cout << "Please enter your age : ";
     // cin >> age;
-    if(cin >> age)
+    while(cin >> age)
     {
         cout << "\n Your age is : " << age;
-    }else{
-        cout << "\n you enter wrong number!";
     }
     
     return 0;
