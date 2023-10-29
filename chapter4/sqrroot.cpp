a46a3413680f28fba1f916ad4f946fcdb9a4b7ec
NARooshnavand <nasserakhoundi@gmail.com>
2023-10-29 01:18:45 -0700

    7-8-1402 third commit

--- chapter4/sqrroot.cpp
+++ chapter4/sqrroot.cpp
@@ -7,11 +7,15 @@ int main()
     cout << "please enter a number : \n";
     cin >> a;
     alpha = a;
-    for(int cnt=0;cnt<5;cnt++)
+    atest = alpha*alpha;
+    int cnt = 0;
+    while(abs(atest-a) > 1e-3)
     {
-        atest = alpha*alpha;
-        cout << "\n approximation of sqrt : " << a << " in iteration " << cnt << " is " << atest;
+        cnt++;
+        cout << "\n approximation of sqrt : " << a << " in iteration " 
+        << cnt << " is " << atest;
         alpha = alpha/2 + a/(2*alpha);
+        atest = alpha * alpha;
     }
     cout << "The sqrt of : " << a << " is : " << alpha;
     atest = alpha*alpha;
