d5ef450c39e062e928b3fff9b7847cf547a56845
NARooshnavand <nasserakhoundi@gmail.com>
2023-11-06 23:43:24 -0800

    16-8-1402 third commit

--- chapter4/printPrimes.cpp
+++ chapter4/printPrimes.cpp
@@ -2,12 +2,23 @@
 #include "Math.h"
 using namespace std;
 int main(){
-    int n;
+    int n,cnt{2};
     string message;
     Math math;
     cout << "Please enter the number : ";
     cin >> n;
+    while(cnt<=n)
+    {
+        message = math.isPrime(cnt)? to_string(cnt)+" ": "";
+        cout << message;
+        // if(math.isPrime(cnt))
+        // {
+        //     cout << cnt << " ";
+        // }
+        cnt++;
+    }
+
     // math.isPrime(n)? message = "is Prime": message = "not Prime";
-    message = math.isPrime(n)? "is Prime": "is not Prime";
-    cout << "The number " << n << " "<< message; 
+    // message = math.isPrime(n)? "is Prime": "is not Prime";
+    // cout << "The number " << n << " "<< message; 
 }
