9c986df98ad4334e0f98d1aab3bfeedb3215e01b
NARooshnavand <nasserakhoundi@gmail.com>
2023-11-06 23:59:51 -0800

    16-8-1402 fourth commit

--- chapter4/main.cpp
+++ chapter4/main.cpp
@@ -4,22 +4,30 @@ using namespace std;
 
 int main()
 {
-    int score=0;
+    int score=0,grade;
     cout << " Please enter the score : ";
     cin >> score;
-    // (score >= 90)? cout << "A" : cout << "not A"; 
-    if(score >= 90)
-    {
-        cout << " A";
-    }else if(score >= 80){
-        cout << "B";
-    }else if(score >= 70){
-        cout << "C";
-    }else if(score >= 60){
-        cout << "D";
-    }else if(score >= 50){
-        cout << "E";
-    }else{
-        cout << "F";
-    }
+    grade = score/10;
+    // (score >= 90)? cout << "A" : cout << "not A";
+    switch(grade){
+        case 9:
+            cout << " A";
+            break;
+        case 8:
+            cout << " B";
+            break;
+        case 7:
+            cout << " C";
+            break;
+        case 6:
+            cout << " D";
+            break;
+        case 5:
+            cout << " E";
+            break;
+        default:
+            cout << "F";
+            break;
+    } 
+    
 }
--- chapter4/printPrimes.cpp
+++ chapter4/printPrimes.cpp
@@ -2,21 +2,19 @@
 #include "Math.h"
 using namespace std;
 int main(){
-    int n,cnt{2};
+    int n;
     string message;
     Math math;
     cout << "Please enter the number : ";
     cin >> n;
-    while(cnt<=n)
+    for(int i=2;i<=n;i=i+3)
     {
-        message = math.isPrime(cnt)? to_string(cnt)+" ": "";
-        cout << message;
-        // if(math.isPrime(cnt))
-        // {
-        //     cout << cnt << " ";
-        // }
-        cnt++;
+        if(math.isPrime(i))
+        {
+            cout << i << " ";
+        }
     }
+    
 
     // math.isPrime(n)? message = "is Prime": message = "not Prime";
     // message = math.isPrime(n)? "is Prime": "is not Prime";
