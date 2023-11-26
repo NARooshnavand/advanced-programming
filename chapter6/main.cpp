4328a01f25b6ce3fc16d2c9b353036dc8296e527
NARooshnavand <nasserakhoundi@gmail.com>
2023-11-25 23:06:15 -0800

    chapter 6 final

--- chapter6/main.cpp
+++ chapter6/main.cpp
@@ -3,8 +3,39 @@
 #include<cstdlib>
 
 using namespace std;
-
+void  useLocal();
+void  useStaticLocal();
+void useGlobalLocal();
+int x{1};
 int main(){
-    srand(time(0));
-    cout << (1+rand()%15);
-}
+    int x{10};
+    cout << "local variable x : "<<x << endl;
+    {
+        int x{15};
+        cout << "new local inside block varible x : "<< x<<endl;
+    };
+    cout << "glibal variable x : "<<::x << endl;
+    useLocal();
+    useStaticLocal();
+    useGlobalLocal();
+    useLocal();
+    useStaticLocal();
+    useGlobalLocal();
+};
+void useLocal(){
+    int x{20};
+    cout << "x inside useLocal is : " << x << endl;
+    ++x;
+    cout << "x inside useLocal after increment is : " << x << endl;
+};
+void useStaticLocal(){
+    static int x{50};
+    cout << "x inside usestaticLocal is : " << x << endl;
+    ++x;
+    cout << "x inside usestaticLocal after increment is : " << x << endl;
+};
+void useGlobalLocal(){
+    cout << "x inside useGlobalLocal is : " << x << endl;
+    ++x;
+    cout << "x inside useGlobalLocal after increment is : " << x << endl;
+};
